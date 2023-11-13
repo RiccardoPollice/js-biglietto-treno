4b94f56f9ceb4a6db7d28868d44745ac8e4fb99e
RiccardoPollice <riccardo.pollice17@gmail.com>
2023-11-13 16:07:40 +0100

    Fine del progetto. Ho aggiunto anche una foto e risolto problema dello sconto non applicato.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 let distance= parseInt(prompt("Inserisci quanti km vuoi percorrere"));
 let age= parseInt(prompt("Inserisci la tua età"));
-let ticketprice= (distance * 0.21);
-let finalprice= ticketprice.toFixed(2);
+let ticketPrice= (distance * 0.21);
 
-if (age>=65) {
-    ticketprice- (ticketprice * 0.60);
+if (age >= 65) {
+    ticketPrice= ticketPrice * 0.6
 }
 else if (age<18) {
-    ticketprice- (ticketprice * 0.20);
-}
-else {
-    ticketprice = ticketprice;
+    ticketPrice= ticketPrice * 0.8
 }
 
-console.log ("Il prezzo del biglietto è:" + finalprice);
+let finalPrice= ticketPrice.toFixed(2);
+
+console.log ("Il prezzo del biglietto è:" + finalPrice);
+document.getElementById("final").innerHTML = finalPrice
